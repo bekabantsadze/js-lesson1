391812b63eb6a414b17fc08edcfdfbb324525819
Beka Bantasdze <thelataito@gmail.com>
2022-10-21 00:10:39 +0400

    test commit

---
+++ script.js
@@ -0,0 +1,83 @@
+
+// davaleba 1 //
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age:25,
+//     studentstatus:"active",
+// }
+
+// console.log(user.studentstatus);
+
+
+
+// davaleba 2 //
+//  let user1 = ["beka","sandro","giga","lena","andria"]
+//     for (i=0; i<user1.length; i++){
+//         console.log(user1[i]);
+//     }
+ 
+
+
+// let i = 0
+
+// while (i <user1.length){
+//     console.log(user1[i]);
+//     i++
+// }
+
+ 
+//  davaleba 3//
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+//     for(i=0; i<numbers.length; i++){
+//         if(numbers[i] > 5) {
+//             console.log(numbers[i]);
+//         }
+            
+       
+//     }
+// davaleba 4
+
+// let user3 = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+
+// if(user3.age<18 && studentstatus === "active"){
+//      console.log('hello')
+// }
+
+// if(user3.name === "levan"){
+//      console.log('hellolevan');
+// }
+// if(user3.studentstatus === "active" || user3.age <25) {
+//     console.log('helloworld');
+
+// }
+// else{console.log("error")}
+    
+// davaleba 5 
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+   
+// for(i=0; i<array.length; i++)
+//     if(typeof array[i] === "string"){
+//         console.log(array[i]);
+//     }
+    
+// //    davaleba 6
+
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+    
+// for(i=0; i<array.length; i++){
+//      for(j=0; j<array[i].length; j++){
+//          if(array[i][j]>0)
+//         console.log(array[i][j]);
+//      }
+       
+}
+
+   
+
